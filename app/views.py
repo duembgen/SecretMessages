0c52e9cce84a19218f4ae33eae23cefb53df04fd
Dümbgen Frederike <duembgen@intranet.epfl.ch>
2017-11-04 01:13:58 +0100

    Adjusted naming.

--- app/views.py
+++ app/views.py
@@ -2,8 +2,7 @@ import os, sys
 import time
 from flask import render_template, request, redirect, url_for, g
 from werkzeug.utils import secure_filename
-from app import app #, models #, db
-#from models import Page
+from app import app 
 
 @app.route('/')
 @app.route('/index')
@@ -11,20 +10,12 @@ def index():
     print('showing index...')
     return render_template('index.html', title='Home')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/messages', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(path)
+        text = request.files['file']
+        print('recorded:',text)
         return redirect(url_for('index'))
-    return render_template('upload.html')
-
-@app.route('/keyword_mapping')
-@app.route('/keyword_mapping', methods=['GET','POST'])
-def keyword_entry():
-    return render_template('keyword_mapping.html', pages=pages)
+    return render_template('messages.html')
